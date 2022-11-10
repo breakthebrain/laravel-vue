dca122105a95b92393583d11dd7acf42826e5d53
Alexandr <breakthebrain1@gmail.com>
2022-11-10 09:41:10 +0200

    add votes, answers and views counter

--- app/Question.php
+++ app/Question.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $fillable = ['title', 'body'];
-    
-    public function user() {
+
+    public function user()
+    {
         return $this->belongsTo(User::class);
-    }    
+    }
 
     public function setTitleAttribute($value)
     {
@@ -26,5 +27,16 @@ class Question extends Model
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
     }
 }
